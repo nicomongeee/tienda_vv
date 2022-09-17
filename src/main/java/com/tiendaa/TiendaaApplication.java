5ef87bd0e923494e7e7085826cc952798b95dc9c
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2022-09-17 10:09:19 -0600

    Semana 1, proyecto inicial del curso Viernes IIIC 2022

---
+++ src/main/java/com/tiendaa/TiendaaApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaa/TiendaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
