111c5b003e95d16ed2cf2f43fbadc3d00ab36008
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2022-10-27 21:33:02 -0600

    Creacion de las plantillas

--- src/main/java/com/tiendaa/services/ClienteServiceImpl.java
+++ src/main/java/com/tiendaa/services/ClienteServiceImpl.java
@@ -8,34 +8,36 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service //para que la clase se considere un servicio
-public class ClienteServiceImpl implements ClienteService{
-    
+public class ClienteServiceImpl implements ClienteService {
+
     //@Autowired provoca que si el objeto ya esta en memoria se usa ese
     //Si no esta en memoria se crea el objeto
     @Autowired
     private ClienteDao clienteDao;
-    
+
     //metodos para hacer un CRUD
     //CREATE READ UPDATE DELETE
-
     @Override
     @Transactional(readOnly = true)
-    public List<Cliente> getClientes(){
-        return (List<Cliente>)clienteDao.findAll();
+    public List<Cliente> getClientes() {
+        return (List<Cliente>) clienteDao.findAll();
     }
+
     @Override
     @Transactional(readOnly = true) //mientras tenga el readonly no modifica en la BD
-    public Cliente getCliente(Cliente cliente){
+    public Cliente getCliente(Cliente cliente) {
         return clienteDao.findById(cliente.getIdCliente()).orElse(null);
     }
+
     @Override
     @Transactional
-    public void save(Cliente cliente){ //insertar o actualizar
-       clienteDao.save(cliente);
+    public void save(Cliente cliente) { //insertar o actualizar
+        clienteDao.save(cliente);
     }
+
     @Override
     @Transactional
-    public void delete(Cliente cliente){
+    public void delete(Cliente cliente) {
         clienteDao.delete(cliente);
     }
 }
